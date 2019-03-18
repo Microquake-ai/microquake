cf7673099878040e673dfaa0f0e3bb066b759c67
Mike Hagerty <m.hagerty@isti.com>
2019-03-18 11:35:13 -0400

    Moved focal mechanism calc (needs hashwrap pkg) into microquake

--- microquake/focmec/core.py
+++ microquake/focmec/core.py
@@ -1,109 +1,167 @@
-"""
-Package to interact with the FOCMEC sofware (Snoke 2003)
-"""
 
-def incidence_phase_vectors(station, origin, velocity=None):
+import numpy as np
+
+#from hashwrap.hashwrapper import calc_focal_mechanisms as calc_HASH_focal_mechanisms
+
+from hashwrap import hashwrapper
+
+from obspy.core.event.source import FocalMechanism, NodalPlane, NodalPlanes
+from obspy.imaging.beachball import aux_plane
+from obspy.core.event.base import Comment
+
+import logging
+logger = logging.getLogger(__name__)
+
+def calc_focal_mechanisms(cat, settings, logger_in=None):
+
+    fname = 'calc_focal_mechanism'
+
+    global logger
+    if logger_in is not None:
+        logger = logger_in
+
     """
-    calculate the incidence vector for the P-, SV- and SH- phase at a station
-    for a wave originating from an event given a potentially complex 3D velocity
-    model.
-    .. NOTE: Straight rays are assumed if no velocity model is provided or if
-    the length of the unique value in the velocity model is 1
-    :param station: station information
-    :type station: ~microquake.core.data.station.Station
-    :param velocity: velocity grid
-    :type velocity: ~microquake.core.data.grid.GridData
-    :param origin: hypocenter location information
-    :type origin: ~microquake.core.event.Origin
-    :rtype: tuple of P, SV and SH vectors
-    """
-    from microquake.simul.eik import eikonal_solver
-    import numpy as np
-    tt = eikonal_solver(velocity, origin.loc, "event")
-    # finding the direction of stepest ascent that is the direction of the
-    # gradient if the velocity is not homogeneous
-    try:
-        unique_values = len(np.unique(velocity.data))
-    except:
-        unique_values = 1
-
-    if velocity or (unique_values > 1):
-        tt_gds = np.gradient(tt.data)
-
-        # calculating the incidence vector for the P wave
-        P = []
-        for tt_gd in tt_gds:
-            gd_tmp = tt.copy()
-            gd_tmp.data = tt_gd
-            P.append(gd_tmp.interpolate(station.loc,
-                grid_coordinate=False)[0])
-
-    else:
-        # assuming straight ray
-        P = station.loc - origin.loc
-
-    # P, SH, SV is a right handed coordinate system
-    P = np.array(P)
-    P /= np.linalg.norm(P)
-
-    SH = np.array([-P[1], P[0], 0])
-    SH /= np.linalg.norm(SH)
-
-    SV = np.cross(P, SH)
-    SV /= np.linalg.norm(SV)
-
-    return (P, SV, SH)
-
-
-def rotate_P_SV_SH(incidence_vects, station):
-    """
-    rotate into P, SV and SH
+    Prepare input arrays needed to run hashwrapper.calc_focal_mechanisms
     """
 
-def measure_wave_polarity(site, velocity, origin, picks, stream, wave_type='P'):
+    sname  = []
+    p_pol  = []
+    p_qual = []
+    qdist = []
+    qazi = []
+    qthe = []
+    sazi = []
+    sthe = []
+
+
+    events = []
+
+    for event in cat:
+
+        event_dict = {}
+
+        origin = event.preferred_origin()
+
+        event_dict['event_info'] = origin.time.datetime.strftime('%Y-%m-%d %H:%M:%S')
+        event_dict['event'] = {}
+        event_dict['event']['qdep']= origin.loc[2]
+        event_dict['event']['sez']= 10.
+        event_dict['event']['icusp']= 1234567
+
+        arrivals = [arr for arr in event.preferred_origin().arrivals if arr.phase == 'P']
+
+        for arr in arrivals:
+
+            if arr.pulse_snr is None:
+                logger.warn("%s P arr pulse_snr == NONE !!!" % \
+                      arr.pick_id.get_referred_object().waveform_id.station_code)
+                continue
+
+            sname.append(arr.pick_id.get_referred_object().waveform_id.station_code)
+            p_pol.append(arr.polarity)
+            qdist.append(arr.distance)
+            qazi.append(arr.azimuth)
+    # MTH: both HASH and test_stereo expect takeoff theta measured wrt vertical Up!
+            qthe.append(180. - arr.takeoff_angle)
+            sazi.append(2.)
+            sthe.append(10.)
+
+            if arr.pulse_snr >= 100.:
+                qual = 0
+            else:
+                qual = 1
+            p_qual.append(qual)
+
+        event_dict['sname'] = sname
+        event_dict['p_pol'] = p_pol
+        event_dict['p_qual'] = p_qual
+        event_dict['qdist'] = qdist
+        event_dict['qazi'] = qazi
+        event_dict['qthe'] = qthe
+        event_dict['sazi'] = sazi
+        event_dict['sthe'] = sthe
+
+    events.append(event_dict)
+
     """
-    calculate the arrival vector at a station for both P-, SV- and SH- waves
-    given an event location and a series of picks.
-    :param site: information on the station network
-    :type site: ~microquake.core.data.station.Site
-    :param velocity: P- or S-wave velocity grid
-    :type velocity: ~microquake.core.data.grid.GridData
-    :param origin: event hypocenter information
-    :type origin: ~microquake.core.event.Origin
-    :param picks: picks associated to the origin
-    :type picks: ~microquake.core.event.Picks
-    :param stream: seismogram of the event
-    :type stream: ~microquake.core.stream.Stream
-    :param wave_type: Phase ('P'. 'SV' or 'SH')
-    :type wave_type: str
+    for azi in sorted(events[0]['qazi']):
+        print(azi)
+    exit()
     """
 
-    # calculate travel time grid from the event location
-    from microquake.simul.eik import eikonal_solver
+    outputs = hashwrapper.calc_focal_mechanisms(events, settings, phase_format='FPFIT')
 
-    tt = eikonal_solver(velocity, origin.loc)
+    focal_mechanisms = []
+
+    plot_figures = []
+    for i,out in enumerate(outputs):
+        logger.info("%s.%s: Process Focal Mech i=%d" % (__name__,fname, i))
+        p1 = NodalPlane(strike=out['strike'], dip=out['dip'], rake=out['rake'])
+        s,d,r = aux_plane(out['strike'], out['dip'], out['rake'])
+        p2 = NodalPlane(strike=s, dip=d, rake=r)
+
+        fc = FocalMechanism(nodal_planes = NodalPlanes(nodal_plane_1=p1, nodal_plane_2=p2),
+                            azimuthal_gap = out['azim_gap'],
+                            station_polarity_count = out['station_polarity_count'],
+                            station_distribution_ratio = out['stdr'],
+                            misfit = out['misfit'],
+                            evaluation_mode = 'automatic',
+                            evaluation_status = 'preliminary',
+                            comments = [Comment(text="HASH v1.2 Quality=[%s]" % out['quality'])]
+                           )
+        logger.info(fc)
+        focal_mechanisms.append(fc)
+
+        event = events[i]
+
+        title = "%s (s,d,r)_1=(%.1f,%.1f,%.1f) _2=(%.1f,%.1f,%.1f)" % \
+                (event['event_info'], p1.strike, p1.dip, p1.rake, p2.strike,p2.dip,p2.rake)
+
+        gcf = test_stereo(np.array(event['qazi']),np.array(event['qthe']),np.array(event['p_pol']),\
+                    sdr=[p1.strike,p1.dip,p1.rake], title=title)
+
+        plot_figures.append(gcf)
+
+    return focal_mechanisms, plot_figures
 
 
-def calculate_polarity(station, travel_time, origin, pick):
-    """
-    Convert first motion polarity as mesured along a given sensor
-    component into P or S-wave polarity (depending on the pick phase hint).
-    The positive motion direction is defined as the vector moving away from the
-    source (compression).
+import matplotlib.pyplot as plt
+import mplstereonet
+from obspy.imaging.beachball import aux_plane
 
-    :param travel_time: travel time grid calculate with the station as seed
-    :type travel_time: ~microquake.core.data.grid.GridData
-    :param station: station information
-    :type station: ~microquake.core.data.station.Station
-    :param origin: event origin
-    :type origin: ~microquake.core.event.Origin
-    :param pick: pick information
-    :type pick: ~microquake.core.event.Pick
-    :rtype: bool
-    """
+def test_stereo(azimuths,takeoffs,polarities,sdr=[], title=None):
+    '''
+        Plots points with given azimuths, takeoff angles, and
+        polarities on a stereonet. Will also plot both planes
+        of a double-couple given a strike/dip/rake
+    '''
 
-    # the vector from the station to the event need to be calculated. This will
-    # involve calculating a travel time grid from the velocity model.
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='stereonet')
+    up = polarities > 0
+    dn = polarities < 0
+    #h_rk = ax.rake(azimuths[up]-90.,takeoffs[up],90, 'ro')
+    # MTH: this seems to put the observations in the right location
+    #  We're plotting a lower-hemisphere focal mech, and we have to convert
+    #  the up-going rays to the right az/dip quadrants:
+    h_rk = ax.rake(azimuths[up]-90.+180.,90.-takeoffs[up],90, 'ro')
+    h_rk = ax.rake(azimuths[dn]-90.+180.,90.-takeoffs[dn],90, 'b+')
+    #ax.rake(strike-90., 90.-dip, rake, 'ro', markersize=14)
 
-    pass
+    #h_rk = ax.rake(azimuths[dn]-90.,takeoffs[dn],90, 'b+')
+    if sdr:
+        s2,d2,r2 = aux_plane(*sdr)
+        h_rk = ax.plane(sdr[0],sdr[1],'g')
+        h_rk = ax.rake(sdr[0],sdr[1],-sdr[2], 'go')
+        h_rk = ax.plane(s2,d2, 'g')
 
+    if title:
+        plt.title(title)
+
+    #plt.show()
+
+    return plt.gcf()
+
+
+if __name__ == '__main__':
+    main()
