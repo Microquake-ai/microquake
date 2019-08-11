3e958338bbbc13eb8971a251dff94becd7ee120f
Reza Jelveh <toyb0mb@gmx.net>
2019-08-11 16:47:00 +0800

    refactor: move connector code to microquake

---
+++ microquake/db/connectors.py
@@ -0,0 +1,169 @@
+from datetime import datetime
+from time import time
+
+import numpy as np
+import sqlalchemy as db
+from pytz import utc
+from sqlalchemy.orm import sessionmaker
+
+from loguru import logger
+from obspy.core import UTCDateTime
+from microquake.core.stream import Stream, Trace
+from redis import ConnectionPool, Redis
+from rq import Queue
+from microquake.db.models import Recording
+from microquake.core.settings import settings
+from spp.stats_collector import db_models
+from spp.stats_collector.db_models import processing_logs
+
+db_name = settings.POSTGRES_DB_NAME
+postgres_url = settings.POSTGRES_URL + db_name
+redis_url = settings.REDIS_URL
+
+
+def connect_redis():
+    return RedisWrapper().redis_connect(url=redis_url)
+
+
+class RedisWrapper(object):
+    shared_state = {}
+
+    def __init__(self):
+        self.__dict__ = self.shared_state
+
+    def redis_connect(self, url):
+        connection_pool = ConnectionPool(redis_url)
+
+        return Redis(connection_pool=connection_pool)
+
+
+class RedisQueue:
+    def __init__(self, queue, timeout=600):
+        self.redis = connect_redis()
+        self.timeout = timeout
+        self.queue = queue
+        self.rq_queue = Queue(self.queue, connection=self.redis,
+                              default_timeout=self.timeout)
+
+    def submit_task(self, func, *args, **kwargs):
+        return self.rq_queue.enqueue(func, *args, **kwargs)
+
+
+# def submit_task_to_rq(queue, func, *args, **kwargs):
+#     with connect_redis() as redis:
+#         rq_queue = Queue(queue, connection=redis)
+#         return rq_queue.enqueue(func, *args, **kwargs)
+
+# rq worker --url redis://redisdb:6379 --log-format '%(asctime)s '  api
+
+def connect_rq(message_queue):
+    redis = connect_redis()
+
+    return Queue(message_queue, connection=redis)
+
+
+def connect_postgres():
+
+    engine = db.create_engine(postgres_url)
+    connection = engine.connect()
+    # Create tables if they do not exist
+    db_models.metadata.create_all(engine)
+
+    return connection
+
+
+def create_postgres_session():
+    engine = db.create_engine(postgres_url)
+    pg = connect_postgres()
+    Session = sessionmaker(bind=engine)
+
+    return Session()
+
+
+def get_continuous_data(starttime, endtime, sensor_id=None):
+
+    session = create_postgres_session()
+
+    t0 = time()
+
+    if sensor_id is not None:
+        session.query(Recording).filter(
+            Recording.time <= endtime).filter(
+            Recording.end_time >= starttime).filter(
+            Recording.sensor_id == sensor_id).all()
+
+    else:
+        results = session.query(Recording).filter(
+            Recording.time <= endtime).filter(
+            Recording.end_time >= starttime).all()
+
+    t1 = time()
+    logger.info('retrieving the data in {} seconds'.format(t1 - t0))
+
+    trs = []
+
+    for trace in results:
+        tr = Trace()
+
+        for channel in ['x', 'y', 'z']:
+
+            if np.all(trace.__dict__[channel] == 0):
+                continue
+
+            tr.stats.network = settings.NETWORK_CODE
+            tr.stats.station = str(trace.sensor_id)
+            tr.stats.location = ''
+            tr.stats.channel = channel
+            tr.stats.sampling_rate = trace.sample_rate
+            tr.stats.starttime = UTCDateTime(trace.time)
+            tr.data = np.array(trace.__dict__[channel])
+            trs.append(tr)
+
+    st = Stream(traces=trs)
+    st.trim(starttime=UTCDateTime(starttime), endtime=UTCDateTime(endtime),
+            pad=False, fill_value=0)
+
+    return st
+
+
+def record_processing_logs_pg(event, status, processing_step,
+                              processing_step_id, processing_time_second):
+    """
+    Record the processing logs in the postgres database
+    :param event: event being processed
+    :param status: processing status (accepted values are success, failed)
+    :param processing_step: processing step name
+    :param processing_step_id: processing step identifier integer
+    :param processing_time_second: processing dealy for this step in seconds
+    :param processing_time_second: processing time for this step in seconds
+    :return:
+    """
+
+    origin = event.preferred_origin()
+
+    if origin is None:
+        origin = event.origins[-1]
+
+    event_time = origin.time.datetime.replace(tzinfo=utc)
+
+    processing_time = datetime.utcnow().replace(tzinfo=utc)
+    processing_delay_second = (processing_time - event_time).total_seconds()
+
+    document = {'event_id': event.resource_id.id,
+                'event_timestamp': event_time,
+                'processing_timestamp': processing_time,
+                'processing_step_name': processing_step,
+                'processing_step_id': processing_step_id,
+                'processing_delay_second': processing_delay_second,
+                'processing_time_second': processing_time_second,
+                'processing_status': status}
+
+    with connect_postgres() as pg:
+        query = db.insert(processing_logs)
+        values_list = [document]
+
+        result = pg.execute(query, values_list)
+
+        pg.close()
+
+    return result
---
+++ microquake/db/models.py
@@ -0,0 +1,74 @@
+import sqlalchemy as db
+metadata = db.MetaData()
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+from sqlalchemy import Column, Integer, String
+
+processing_logs = db.Table('processing_logs', metadata,
+                            db.Column('event_id',db.String(255),
+                                      primary_key=True),
+                            db.Column('event_timestamp', db.DateTime),
+                            db.Column('processing_timestamp', db.DateTime),
+                            db.Column('processing_step_name', db.String(255)),
+                            db.Column('processing_step_id', db.Integer),
+                            db.Column('processing_delay_second', db.Float),
+                            db.Column('processing_time_second', db.Float),
+                            db.Column('processing_status', db.String(255)))
+
+processing = db.Table('processing', metadata,
+                      db.Column('event_id', db.String(255), primary_key=True),
+                      db.Column('P_picks', db.Integer),
+                      db.Column('S_picks', db.Integer),
+                      db.Column('processing_delay_second', db.Float),
+                      db.Column('processing_completed_timestamp',
+                                db.Float),
+                      db.Column('event_category', db.String(255)),
+                      db.Column('event_status', db.String(255)))
+
+# class data_quality(Base):
+#     __tablename__ = 'data_quality'
+#
+#     id = Column(db.Integer, primary_key=True)
+#     time = Column(db.DateTime, nullable=False)
+#     processing_time = Column(db.DateTime, nullable=True)
+#     sensor_id = Column(db.Integer, nullable=False)
+#     data_quality_index = Column
+# data_quality = db.Table('data_quality', metadata,
+#                         db.Column('timestamp', db.DateTime),
+#                         db.Column('processing_timestamp', db.DateTime),
+#                         db.Column('data_quality_index', db.Float),
+#                         db.Column('station', db.String(8)),
+#                         db.Column('location', db.String(8)),
+#                         db.Column('component', db.String(3)),
+#                         db.Column('percent_recovery', db.Float),
+#                         db.Column('signal_std', db.Float),
+#                         db.Column('signal_energy', db.Float),
+#                         db.Column('signal_max_amplitude', db.Float),
+#                         db.Column('signal_dominant_frequency', db.Float),
+#                         db.Column('average_cross_correlation', db.Float))
+
+
+# recordings = db.Table('recordings', metadata,
+#                      Column('id', db.Integer, primary_key=True),
+#                      Column('time', db.DateTime),
+#                      Column('sensor_id', db.Integer),
+#                      Column('sample_count', db.Integer),
+#                      Column('sample_rate', db.Float)
+#                      # Column('x', db.(db.Float), nullable=False),
+#                      # Column('y', db.ARRAY(db.Float), nullable=False),
+#                      # Column('z', db.ARRAY(db.Float), nullable=False)
+#                      )
+
+
+class Recording(Base):
+    __tablename__ = 'recordings'
+
+    time = Column(db.DateTime(timezone=True), primary_key=True)
+    end_time = Column(db.DateTime(timezone=True))
+    sensor_id = Column(db.Integer)
+    sensor_type_id = Column(db.Integer)
+    sample_count = Column(db.Integer)
+    sample_rate = Column(db.Float)
+    x = Column(db.Float)
+    y = Column(db.Float)
+    z = Column(db.Float)
