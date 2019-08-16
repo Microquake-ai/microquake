fe57b671190d271c1e08a122c99dca596239f6bb
Reza Jelveh <toyb0mb@gmx.net>
2019-08-16 23:23:21 +0800

    disable ims test with pytest decorator

--- tests/test_get_continuous.py
+++ tests/test_get_continuous.py
@@ -6,17 +6,21 @@ get_continuous(base_url, start_datetime, end_datetime,
                    sampling_rate=6000., nan_limit=10):
 """
 
+from importlib import reload
+
+import pytest
+
 from microquake.clients.ims import web_client
 from microquake.core import UTCDateTime
-from importlib import reload
-reload(web_client)
-
-base_url = 'http://10.95.74.35:8002/ims-database-server/databases/mgl'
-station_ids = ['20']
-start_time = UTCDateTime.now() - 10 * 60
-end_time = UTCDateTime.now() - 8 * 60
-
-st = web_client.get_continuous(base_url, start_time, end_time, station_ids)
 
 
+@pytest.mark.skip(reason="Currently this depends on a url that can't be accessed")
+def test_webclient():
+    reload(web_client)
 
+    base_url = 'http://10.95.74.35:8002/ims-database-server/databases/mgl'
+    station_ids = ['20']
+    start_time = UTCDateTime.now() - 10 * 60
+    end_time = UTCDateTime.now() - 8 * 60
+
+    web_client.get_continuous(base_url, start_time, end_time, station_ids)
