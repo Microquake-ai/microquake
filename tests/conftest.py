fc2a19015ab86440c3443125e4f0ce361bc8fb0d
Reza Jelveh <toyb0mb@gmx.net>
2019-08-17 12:43:35 +0800

    further improve testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,19 @@
-# import pytest
+from os import path
 
-# import rom.util
-# from fakeredis import FakeStrictRedis
+import pytest
+
+from fakeredis import FakeStrictRedis
 
 
-# @pytest.fixture(scope="session", autouse=True)
-# def rom_setup():
-#     rom.util.get_connection = FakeStrictRedis
+@pytest.fixture(scope="session", autouse=True)
+def redis():
+    return FakeStrictRedis()
+
+
+@pytest.fixture(scope="session", autouse=True)
+def picks():
+    pick_path = path.join(path.dirname(path.realpath(__file__)), 'fixtures/picks.json')
+    with open(pick_path) as file:
+        data = file.read()
+
+        return data
