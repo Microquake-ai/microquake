893f8d5ee0a85d1f668d2b5e90b545342c001166
Fran Hrzenjak <fran.hrzenjak@gmail.com>
2019-09-12 11:25:30 +0200

    "type" is a built-in function in python, renaming to "grid_type"

--- microquake/core/helpers/grid.py
+++ microquake/core/helpers/grid.py
@@ -15,24 +15,24 @@ from microquake.core.settings import settings
 from microquake.core.simul.eik import ray_tracer
 
 
-def get_grid(station_code, phase, type='time'):
+def get_grid(station_code, phase, grid_type='time'):
     """
     get a travel time grid for a given station and a given phase
     :param station_code: station code
     :param phase: Phase ('P' or 'S')
-    :param type: type of grid ('time', 'take_off', 'azimuth')
+    :param grid_type: type of grid ('time', 'take_off', 'azimuth')
     :return:
     """
     nll_dir = settings.nll_base
     f_tt = os.path.join(nll_dir, 'time', 'OT.%s.%s.%s.buf'
-                        % (phase.upper(), station_code, type))
+                        % (phase.upper(), station_code, grid_type))
     tt_grid = read_grid(f_tt, format='NLLOC')
 
     return tt_grid
 
 
 def get_grid_point(station_code, phase, location,
-                   grid_coordinates=False, type='time'):
+                   grid_coordinates=False, grid_type='time'):
     """
     get value on a grid at a given point inside the grid
     :param station_code: Station code
@@ -40,11 +40,11 @@ def get_grid_point(station_code, phase, location,
     :param location: point where the value is interpolated
     :param grid_coordinates: whether the location is expressed in grid
     coordinates or in model coordinates (default True)
-    :param type: type of grid ('time', 'take_off', 'azimuth')
+    :param grid_type: type of grid ('time', 'take_off', 'azimuth')
     :return:
     """
 
-    tt = get_grid(station_code, phase, type=type)
+    tt = get_grid(station_code, phase, grid_type=grid_type)
 
     return tt.interpolate(location, grid_coordinate=grid_coordinates)[0]
 
@@ -59,7 +59,7 @@ def get_ray(station_code, phase, location, grid_coordinate=False):
     coordinates or model coordinates (default False)
     :return:
     """
-    travel_time = get_grid(station_code, phase, type='time')
+    travel_time = get_grid(station_code, phase, grid_type='time')
 
     return ray_tracer(travel_time, location,
                       grid_coordinates=grid_coordinate)
@@ -111,9 +111,10 @@ def create_arrivals_from_picks(picks, event_location, origin_time):
         # print("create_arrivals: sta:%3s pha:%s pick.time:%s
 
         arrival.takeoff_angle = get_grid_point(station_code, phase,
-                                               event_location, type='take_off')
+                                               event_location,
+                                               grid_type='take_off')
         arrival.azimuth = get_grid_point(station_code, phase,
-                                         event_location, type='azimuth')
+                                         event_location, grid_type='azimuth')
         # print("create arrival: type(arrival)=%s type(takeoff_angle)=%s type(azimuth)=%s" % \
         # (type(arrival), type(arrival.takeoff_angle), type(arrival.azimuth)))
 
