increase the time out in rq to 1800 seconds (30 minutes), the calculation of the magnitude takes a long time when the number of picks is high. Also fixed an issue in the magnitude_extractor.

microquake/processors/magnitude_extractor.py
@@ -92,7 +92,7 @@ class Processor(ProcessingUnit):
             mw = np.mean([td_magnitude, fd_magnitude])
             mw_uncertainty = np.abs(td_magnitude - fd_magnitude)
 
-        elif td_magnitude:
+        elif td_magnitude is None:
             mw = fd_magnitude
             mw_uncertainty = None
 