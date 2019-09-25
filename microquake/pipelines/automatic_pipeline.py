76696b7de3af7a5492fb22894b651af3360d1e7a
Fran Hrzenjak <fran.hrzenjak@gmail.com>
2019-09-25 15:47:59 +0200

    Allow http 201 response to POST

--- microquake/pipelines/automatic_pipeline.py
+++ microquake/pipelines/automatic_pipeline.py
@@ -156,13 +156,11 @@ def post_event_api(event_id, **kwargs):
                                       stream=event['fixed_length'],
                                       tolerance=None,
                                       send_to_bus=False)
-
-    if response.status_code != requests.codes.ok:
+    try:
+        response.raise_for_status()
+    except requests.HTTPError:
         logger.info('request failed, resending to the queue')
-
         result = api_queue.submit_task(post_event_api, event_id=event_id)
-
-
         return result
 
 
