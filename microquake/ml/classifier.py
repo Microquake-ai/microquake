4542da8b65fc131a0c6cc703c12ed75c19877889
Ibrahim EL-Fayoumi <ibrahim.elfayoumi@toptal.com>
2019-10-03 08:06:09 +0000

    changed classnames to generated ones

--- microquake/ml/classifier.py
+++ microquake/ml/classifier.py
@@ -34,8 +34,8 @@ class SeismicModel:
         self.base_directory = Path(settings.common_dir)/'../data/weights'
         # Model was trained at these dimensions
         self.D = (64, 64, 1)
-        self.microquake_class_names = ['anthropogenic event', 'explosion',
-                                       'earthquake', 'quarry blast']
+        self.microquake_class_names = ['anthropogenic event', 
+                                       'earthquake', 'explosion', 'quarry blast']
         self.num_classes = len(self.microquake_class_names)
         self.model_file = self.base_directory/f"{model_name}"
         self.create_model()
