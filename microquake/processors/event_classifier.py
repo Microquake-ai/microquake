2f3cbd3aad8a9ef228d3776f610988532ff9df1f
Jean-Philippe Mercier <jean-philippem@riotinto.com>
2019-10-08 01:25:36 -0700

    putting a bit more logic in the event_classifier processor

--- microquake/processors/event_classifier.py
+++ microquake/processors/event_classifier.py
@@ -41,8 +41,6 @@ class Processor(ProcessingUnit):
         else:
             magnitude = cat[0].preferred_magnitude().mag
 
-
-
         response = self.seismic_model.predict(tr, context_trace,
                                               elevation, magnitude)
         return response
